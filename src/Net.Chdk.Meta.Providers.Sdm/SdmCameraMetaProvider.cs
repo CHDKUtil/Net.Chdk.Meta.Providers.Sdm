d36af31b958c9a692da69438371652784115d5b3
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 02:47:44 +0300

    Replace SoftwareCameraInfo with CameraInfo

--- src/Net.Chdk.Meta.Providers.Sdm/SdmCameraMetaProvider.cs
+++ src/Net.Chdk.Meta.Providers.Sdm/SdmCameraMetaProvider.cs
@@ -1,14 +1,13 @@
-﻿using Net.Chdk.Model.Software;
-using System.IO;
+﻿using System.IO;
 
 namespace Net.Chdk.Meta.Providers.Sdm
 {
     sealed class SdmCameraMetaProvider : ICameraMetaProvider
     {
-        public SoftwareCameraInfo GetCamera(string name)
+        public CameraInfo GetCamera(string name)
         {
             var split = Path.GetFileNameWithoutExtension(name).Split('-');
-            return new SoftwareCameraInfo
+            return new CameraInfo
             {
                 Platform = split[2],
                 Revision = split[3]
